df7c7851db7f031185ad87e0a937f3848073938d
hafzansh <hafizansh.m@gmail.com>
2020-03-24 11:55:49 +0800

    add ipk and sks system

--- InputOutput.java
+++ InputOutput.java
@@ -4,8 +4,20 @@ public class InputOutput{
     public static void main(String [] args){
         Scanner scanner = new Scanner(System.in);
         String nama;
+        int jumlahSKS;
+        double IPK;
+
         System.out.print("Masukkan nama anda: ");
-        nama= scanner.nextLine();
+        nama = scanner.nextLine();
         System.out.println("Hai, " + nama);
+
+        System.out.print("Masukkan jumlah SKS: ");
+        jumlahSKS = scanner.nextInt();
+        
+        System.out.print("Masukkan IPK Sementara: ");
+        IPK = scanner.nextDouble();
+
+        double jumlah = IPK * jumlahSKS;
+        System.out.print("Total Nilai = "+ jumlah);
     }
 }
