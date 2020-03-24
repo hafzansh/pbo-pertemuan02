64db55884829a74246a5f1cdf29b698419634cc8
hafzansh <hafizansh.m@gmail.com>
2020-03-24 12:00:22 +0800

    add alamat to main

---
+++ model/Alamat.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Alamat{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("===========  Alamat  ===========");
+        System.out.println("Kota : Banjarbaru");
+        System.out.println("Jalan: Mr. Cokrokusumo");
+        System.out.println("RT/RW: 06/02");
+        System.out.println("Kec  : Cempaka");
+        System.out.println("Kel  : Bangkal");
+        System.out.println("================================");
+    }
+}
