2c80b8a1ed9d36bdd2d0f1ccb734dd72a22a9bde
hafzansh <hafizansh.m@gmail.com>
2020-03-24 11:37:58 +0800

    memanggil method dari banyak Class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("========  Data Mahasiswa  ========");
+        System.out.println("NPM  : 18630281");
+        System.out.println("Nama : Muhammad Hafiz Anshori");
+        System.out.println("==================================");
+    }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MataKuliah{
+    public static void tampilanInfo(){
+        System.out.println();
+        System.out.println("=========  Mata Kuliah  =========");
+        System.out.println("NIDN : 1103058602");
+        System.out.println("Nama : Permograman Berbasis Objek");
+        System.out.println("=================================");
+    }
+}
